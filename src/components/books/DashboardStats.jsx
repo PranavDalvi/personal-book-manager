65876b66efd4867b8f1574c940e07d02d23e7dcf
Pranav Dalvi <pranav.dalvi@zohomail.in>
2026-03-06 19:16:17 +0530

    fix: tweaking dashboard stats for mobile UI

--- src/components/books/DashboardStats.jsx
+++ src/components/books/DashboardStats.jsx
@@ -9,31 +9,35 @@ export default function DashboardStats({
   wantToRead,
 }) {
   return (
-    <div className="mb-6 flex flex-col justify-between gap-10 sm:flex-row">
-      <Card className="w-full">
-        <CardContent className="p-4">
-          <p className="text-sm text-gray-500">Total Books</p>
-          <p className="text-2xl font-semibold">{total}</p>
-        </CardContent>
-      </Card>
-      <Card className="w-full">
-        <CardContent className="p-4">
-          <p className="text-sm text-gray-500">Currently Reading Books</p>
-          <p className="text-2xl font-semibold">{reading}</p>
-        </CardContent>
-      </Card>
-      <Card className="w-full">
-        <CardContent className="p-4">
-          <p className="text-sm text-gray-500">Completed Books</p>
-          <p className="text-2xl font-semibold">{completed}</p>
-        </CardContent>
-      </Card>
-      <Card className="w-full">
-        <CardContent className="p-4">
-          <p className="text-sm text-gray-500">Want to Read Books</p>
-          <p className="text-2xl font-semibold">{wantToRead}</p>
-        </CardContent>
-      </Card>
+    <div className="mb-6 flex flex-col justify-between gap-6 sm:flex-row">
+      <div className="flex w-full gap-6">
+        <Card className="w-full">
+          <CardContent className="p-4">
+            <p className="text-sm text-gray-500">Total Books</p>
+            <p className="text-2xl font-semibold">{total}</p>
+          </CardContent>
+        </Card>
+        <Card className="w-full">
+          <CardContent className="p-4">
+            <p className="text-sm text-gray-500">Currently Reading Books</p>
+            <p className="text-2xl font-semibold">{reading}</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex w-full gap-6">
+        <Card className="w-full">
+          <CardContent className="p-4">
+            <p className="text-sm text-gray-500">Completed Books</p>
+            <p className="text-2xl font-semibold">{completed}</p>
+          </CardContent>
+        </Card>
+        <Card className="w-full">
+          <CardContent className="p-4">
+            <p className="text-sm text-gray-500">Want to Read Books</p>
+            <p className="text-2xl font-semibold">{wantToRead}</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
